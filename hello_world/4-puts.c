489e43f59491a9e19af859f635b2105b05a5f31b
aallm004 <abigailrosea10@gmail.com>
2024-01-17 23:56:56 +0000

    4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+//Using puts to print a message	
+int puts(const char* Programming is like building a multilingual puzzle);
+
+// Ending with the value 0
+// return 0;
+// }

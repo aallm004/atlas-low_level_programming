107e1c15de8eaa52e39d80700c5dbd0179e24117
aallm004 <abigailrosea10@gmail.com>
2024-01-18 23:33:21 +0000

    sddsdf

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase and uppercase
+ *
+ * Return: 0 (always success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar ('\n');
+
+	char alphabet = 'A';
+
+	while (alphabet <= 'Z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar ('\n');
+	return (0);
+}

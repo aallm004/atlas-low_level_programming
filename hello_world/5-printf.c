72309c1b7458ffc3fb1c36a49d32fa629859d8a2
aallm004 <abigailrosea10@gmail.com>
2024-01-18 17:06:23 +0000

    124

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,4 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-printf (with proper grammar, but the outcome is a piece of art,)
+{
+	printf (with proper grammar, but the outcome is a piece of art,);
+	return (0);
+}

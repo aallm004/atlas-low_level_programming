d23f2763a02474b2e86fe0c92aafba504955b6ab
aallm004 <abigailrosea10@gmail.com>
2024-01-18 00:14:56 +0000

    lsjdf

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-timeout 30 bash -c 'gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c'
+gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
 int main()
 gcc -o
 {
 //Using puts to print a message	
 int puts(const char* Programming is like building a multilingual puzzle);
 
-// Ending with the value 0
-// return 0;
-// }

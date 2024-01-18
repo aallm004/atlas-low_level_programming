38073871c795060c73b77c4bea509248e0783418
aallm004 <abigailrosea10@gmail.com>
2024-01-18 20:11:09 +0000

    sjdkjllj

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@ int main(void)
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+		if (n > 0)
 	{
 			printf("%d is positive\n", (n));
 	}
@@ -22,5 +22,5 @@ int main(void)
 	{	       	
 			printf("%d is negative\n", (n));
 	}
-	return (0);
+		return (0);
 }

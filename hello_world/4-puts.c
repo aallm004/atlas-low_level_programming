13db330aacb362aa7dc2b6ca0876ac2490064e54
aallm004 <abigailrosea10@gmail.com>
2024-01-18 16:59:23 +0000

    123

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
-int main()
-gcc -o
+/**
+* main - puts
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-//Using puts to print a message	
-int puts(const char* Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
 

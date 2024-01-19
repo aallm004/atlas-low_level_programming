ef2f560545a59f60b25b1158fcdee78ae56102f6
aallm004 <abigailrosea10@gmail.com>
2024-01-19 01:07:16 +0000

    slkdj

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase with exceptions
+ *
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <+ 'z')
+	
+	if (letter == 'e' || letter == 'q') {
+		continue;
+	}
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar ('\n');
+	return (0);
+}

baf3995edc925b1e33237c78d5c0fe89b3dea2fb
aallm004 <abigailrosea10@gmail.com>
2024-01-19 22:38:59 +0000

    ee

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_10x - Print alphabet in lowercase 10 times
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
  *
  * Return: 0 (always success)
  */

2c0202aa50273a34d05db7c4203ba813936b63e2
aallm004 <abigailrosea10@gmail.com>
2024-01-19 23:41:56 +0000

    sldj

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _islower - return non-zero if c is a lowercase letter; otherwise, they shall return 0.
+ * _islower - return non-zero if c is a lowercase letter;
  *
  * Return: 0 (always success)
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		{
-			return(1);
-		}
-		return (0);
+	{
+		return(1);
+	}
+	return (0);
 }

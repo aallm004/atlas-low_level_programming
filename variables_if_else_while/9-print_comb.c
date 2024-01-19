5723eea09595d8c431cfe332dd0e6c0e16429011
aallm004 <abigailrosea10@gmail.com>
2024-01-19 02:34:48 +0000

    slj

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all possible combos of single-digit numbers.
+ *
+ * Return: 0 (always success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x=48; x < 58; x++)
+	{
+		putchar(x);
+		if (x < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

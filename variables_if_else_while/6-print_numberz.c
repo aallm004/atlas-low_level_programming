1a4b63743beb35da15fecfd70ef127f4f49b6200
aallm004 <abigailrosea10@gmail.com>
2024-01-19 01:56:09 +0000

    sllkdj

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program that prints al singe digit numbers etc
+ *
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}

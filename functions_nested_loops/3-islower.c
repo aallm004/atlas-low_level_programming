60541db0a41487a86cb03514725e3248c2f9a5f9
aallm004 <abigailrosea10@gmail.com>
2024-01-19 23:33:02 +0000

    sd

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int_islower - return non-zero if c is a lowercase letter; otherwise, they shall return 0.
+ *
+ * Return: 0 (always success)
+ */
+int_islower(int c);
+
+{
+
+		if (c >= 97 && c<= 122)
+		{
+
+		return(1);
+		}
+		return (0);
+}

cffb4038c1a68d7d760d7c421eea6fe4bdb32c8c
aallm004 <abigailrosea10@gmail.com>
2024-01-21 21:08:23 +0000

    dlal

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * abs(int) - write function that computes the absolute value of integer.
+ *
+ * Return: 0 (always success)
+ */
+
+int abs(int)
+{
+	if (n > 1)
+		return (n);
+
+	else
+	{
+	 	n = -1;
+		return (n);
+	}
+}

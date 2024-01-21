6fc8e2527ea575e357f7f625ccaadca5466c4407
aallm004 <abigailrosea10@gmail.com>
2024-01-21 20:44:45 +0000

    sd

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign(int n)- print the sign of a number
+ *
+ * Return: 1 and prints + if n is greater than zero, 0 if n is zero and  -1 and prints "-" if n is less than zero.
+ */
+int print_sign(int n);
+{
+	int test;
+
+	if (n>0)
+	{
+		test = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	(
+	 	test = 0;
+		_putchar('0');
+	}
+	else
+	{
+		test = -1;
+		_putchar('_');
+	}
+	return (0);
+}

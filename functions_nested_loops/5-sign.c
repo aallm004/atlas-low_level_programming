4f385a8333b3f471084a8c6b193a252ae37c87e7
aallm004 <abigailrosea10@gmail.com>
2024-01-21 20:56:37 +0000

    okaaay

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,18 +13,18 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		test = (1);
+		test = 1;
 		_putchar('+');
 	}
 	else if (n == 0)
 	{
-		test = (0);
+		test = 0;
 		_putchar('0');
 	}
 	else
 	{
-		test = (-1);
+		test = -1;
 		_putchar('-');
 	}
-	return test;
+	return (test);
 }

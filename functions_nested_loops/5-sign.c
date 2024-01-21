6914613667cbe9e7005b93dfab55d252d275cd6b
aallm004 <abigailrosea10@gmail.com>
2024-01-21 20:51:34 +0000

    sd

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 and prints + if n is greater than zero, 0 if n is zero and  -1 and prints "-" if n is less than zero.
  */
-int print_sign(int n);
+int print_sign(int n)
 {
 	int test;
 

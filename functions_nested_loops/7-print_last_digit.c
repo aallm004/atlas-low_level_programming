a0cd5c3df6fa38328f00eb4cce30c3fcc772a7e1
aallm004 <abigailrosea10@gmail.com>
2024-01-21 23:28:11 +0000

    sd

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - Write a function that prints the last digit of a number.
+ *
+ * Return: 0 (always success)
+ */
+
+int print_last_digit(int);
+{
+	if (a>= 0 && a <= 10)
+	reutrn (a):
+
+	else
+	{
+		a = _abs(a % 10);
+		return (a);;
+	}
+}

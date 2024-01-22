b7cef855c8985670685b85e2ba4893f9ef524d5d
aallm004 <abigailrosea10@gmail.com>
2024-01-22 19:13:32 +0000

    slkdj

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,35 +12,29 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if ((c / 10) == 0)
+
+			if (b == 0)
 			{
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				if (b != 0)
-				{
-					_putchar(' ');
-					_putchar((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
+				_putchar('0');
 			}
-		}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+		if (c / 10 == 0)
+			_putchar(' ');
+
 		else
-			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b <9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
+			_putchar((c / 10) + '0');
+
+		_putchar((c % 10) + '0');
 		}
-		_putchar ('\n');
+	
+		if (b < 9)
+			_putchar(',');
+		}
+
+		_putchar('\n');
 	}
 }

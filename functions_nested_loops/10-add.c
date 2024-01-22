ae623bf1f2ad0d916d02b1ec3f0c3472473cdbc8
aallm004 <abigailrosea10@gmail.com>
2024-01-22 00:27:47 +0000

    zxvc

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - funtion that adds two integers and returns the result
+ *
+ * Return: 0 (always success)
+ */
+
+int add(int i, int k);
+{
+	return (i + k);
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * time_table - write function that prints the 9 times table, start 0
+ *
+ * Return: 0 (always success)
+ */
+
+void times_table(void);
+
+

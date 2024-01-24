1199d7a4ae584dcaf6983050fe591d9f1324493f
aallm004 <abigailrosea10@gmail.com>
2024-01-24 19:57:51 +0000

    slkj

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,8 +2,7 @@
 /**
  * int mul- Write function that multiplies two integers.
  *
- * Description: function that multiplies two integers. Mul is used to multiply
- * mul - Multiplies two integers
+ *  mul - Multiplies two integers
  * @a: int
  * @b: int
  * @*: multiply

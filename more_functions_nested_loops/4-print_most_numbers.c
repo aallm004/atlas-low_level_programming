e4f2367651355096816f80d9a248cf55d87c0c5a
aallm004 <abigailrosea10@gmail.com>
2024-01-24 21:22:26 +0000

    traling whitespace

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,9 @@
 void print_most_numbers(void)
 {
 	int x;
-	
+
 	for (x = 48; x < 58; x++)
-	{	
+	{
 		if (x != 50 && x != 52)
 		{
 			_putchar(x);
@@ -18,4 +18,3 @@ void print_most_numbers(void)
 	}
 	_putchar('\n');
 }
-

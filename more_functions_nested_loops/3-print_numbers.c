f1a1e2278f69e36349b591234f616c6a721e78c0
aallm004 <abigailrosea10@gmail.com>
2024-01-24 20:10:29 +0000

    numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * description: print numbers from 0 to 9
+ *
+ * Return (0) always success
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0)
+}

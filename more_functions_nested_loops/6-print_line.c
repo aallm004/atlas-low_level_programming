77207f2650082c076dc9d01ecba91ef3e0395051
aallm004 <abigailrosea10@gmail.com>
2024-01-24 23:44:28 +0000

    tr

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,35 +9,35 @@ void print_line(int n)
 {
 	int l, m, o, p;
 
-		for (l = 12; l < 16; l++)
+		for (l = 12; l < 16; l+1)
 		{
 			if (l > 10)
 			{
 				_putchar('_');
 			}
 		}
-                for (m = 12; m < 16; l++)
+                for (m = 12; m < 16; l+1)
                 {
                         if (m > 10)
                         {
                         	_putchar('_');
                         }
                 }
-		for (n = 12; n < 16; l++)
+		for (n = 12; n < 16; l+1)
                 {
                         if (n > 10)
                         {
                         	_putchar('_');
 			}
 		}
-                for (o = 12; o < 16; l++)
+                for (o = 12; o < 16; l+1)
                 {
                         if (o > 10)
                         {
                         	_putchar('_');
                         }
                 }
-                for (p = 12; p < 16; l++)
+                for (p = 12; p < 16; l+1)
                 {
 			if (p > 10)
 			{

2518b2b9cc5c158b2a8cfb3200ed0775021cf620
aallm004 <abigailrosea10@gmail.com>
2024-01-24 20:56:53 +0000

    #4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{	
+		if (x != 50 && x != 52)
+		{
+			_putchar(x);
+		}
+	}
+}
+

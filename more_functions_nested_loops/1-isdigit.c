87fe043bf22e8ed7362a01d73921a8932e5913fb
aallm004 <abigailrosea10@gmail.com>
2024-01-24 19:08:00 +0000

    is c number

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit- check for digit 0 through 9
+ *
+ * Return: 0 (always success)
+ */
+
+int _isdigit(int c);
+{
+	if (c >= 48 && c<= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

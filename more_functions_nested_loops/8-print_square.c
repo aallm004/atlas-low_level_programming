8ad5c16bc5dfb93238357401426d7dc5a986315e
aallm004 <abigailrosea10@gmail.com>
2024-01-25 01:29:27 +0000

    ahh

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ *
+ * Return: Always 0
+ */
+
+void print_square(int size)
+{
+	int a;
+
+	for (a = 1; a <= n; a++)
+		{
+			_putchar ('#')
+		}
+}

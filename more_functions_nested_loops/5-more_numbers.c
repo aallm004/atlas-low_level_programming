34c83e170fea697c3e87e809237c1643f160c1a8
aallm004 <abigailrosea10@gmail.com>
2024-01-25 18:20:55 +0000

    14?

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,11 @@ void more_numbers(void)
 		for (j = 0; j <= 15; j++)
 		{
 			if(j < 10)
-			{
+				_putchar('0' + (j / 10));
+		}
+				_putchar ('0' + (j % 10));
 				_putchar ('0' + j);
-			}
+	}
 				else
 					_putchar ('\n');
-		}
-	}
-}	
+}

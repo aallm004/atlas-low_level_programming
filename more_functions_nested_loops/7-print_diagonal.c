d6bca620008fe3fd2bd83d0d4e405de2b8c3995c
aallm004 <abigailrosea10@gmail.com>
2024-01-25 00:29:09 +0000

    ahhh

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagonal- draw a diagonal line on the terminal
+ *
+ * Return: Always 0
+ */
+
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	for (a = 1; a <= n; a++)
+		_putchar (' ')
+	{
+		for (b = 1; b < a; b++)
+		{
+			_putchar ('\\')
+			_putchar ('\n')

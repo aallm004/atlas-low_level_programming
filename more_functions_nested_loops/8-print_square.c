a42fda855e88c1afc5db3ccbb2a81717d00bb52e
aallm004 <abigailrosea10@gmail.com>
2024-01-25 20:45:52 +0000

    ah

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,15 +7,11 @@
 void print_square(int size)
 {
         int a, b;
-        for (a = 0; a < 10; a++)
-        {
-                for (a = 1; a <= size; a++)
-                {
-                        for (b = 1; b < a; b++)
-                        {
-                        _putchar('#');
-                        }
-                }
-                _putchar ('\n');
-        }
+	if (a >= size)
+		_putcher ('\n');
+	else
+		for(a = 0; a < size; a++)
+		{
+			_putchar ('#')
+		}
 }

b4e9bf41048da58318e63a9805efec496dbf12e0
aallm004 <abigailrosea10@gmail.com>
2024-01-25 17:58:01 +0000

    ahh

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,12 @@ void more_numbers(void)
 	int i, j;
 
 	for (i = 0; i <= 10; i++)
-	{
-		for (j = 0; j <= 15; j++)
 		{
-			if(j < 10,)
-				_putchar(j/ 10 + '0');
-				_putchar(j % 10 + '0');
+		for (j = 0; j <= 15; j++)
 			{
-				_putchar ('0' + j);
+			if(j < 10,)
+			
+				_putchar (\n);
 			}
 		}
 	}

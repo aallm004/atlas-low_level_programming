2e624c309b5dbd75c86226f85b73fa1ddd3b8cd8
aallm004 <abigailrosea10@gmail.com>
2024-01-26 18:43:06 +0000

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include <std.io>
+/**
+ * print_triangle- function that prints a triangle
+ *
+ * Return: 0
+ */
+
+print_triangle(int size)
+{
+	int r, c;
+
+	if (r = 5; r <= 10; r++)
+	{
+		for(c = 1; c <= 10; c++)
+		{
+			if (c >=(11-r))
+			{
+				_putchar('#')
+			}
+			else
+			{
+				_putchar( )
+			}
+
+			{	
+				_putchar('/n')
+			}
+				
+				if(col==5)
+				{
+					_putchar('#')
+				}
+}

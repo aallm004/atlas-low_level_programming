8b7db9919117b7f0ac45de86b055810500a4b660
aallm004 <abigailrosea10@gmail.com>
2024-01-28 21:15:44 +0000

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include <main.h>
+/**
+ * reset_to_98 - function takes pointer to an int as parameter and updates value to 98.
+ */
+
+void reset_to_98(int *n)
+
+98 = n*
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning how to use pointers, learning about memory, and learning about other things that I don't even know of yet.

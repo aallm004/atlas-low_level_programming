d15376b6d947a3399adf681831adbc7efeb0378e
aallm004 <abigailrosea10@gmail.com>
2024-01-28 21:50:31 +0000

    1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps values of two ints
+ * @a : int
+ * @b : int
+ */
+
+void swap_int(int *a, int *b)
+{
+	temp := a
+	a := b
+	b := temp
+}

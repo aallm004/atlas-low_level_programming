cad5fb38963d2ac8a17728205345d7836d5adfdb
aallm004 <abigailrosea10@gmail.com>
2024-01-29 00:36:02 +0000

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by new line
+ * @str: pointer
+ */
+
+
+void _puts(char *str)
+
+while (*str != '\0')
+{		
+	putchar (*str);
+		str++;
+}

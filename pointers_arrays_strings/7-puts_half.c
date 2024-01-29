7074f8651f6fc66b1e94170b32d6534f4327ffda
aallm004 <abigailrosea10@gmail.com>
2024-01-29 23:13:08 +0000

    ahh

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ *puts_half- f that prints half of a string
+ *@str: string
+ */
+
+void puts_half(char *str)
+
+	int len, x, i;
+
+	len = 0;
+
+	for (i = len / 2; str[i] != '\0'; i++)
+
+
+
+
+
+
+
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s : int
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	return (x);
+}

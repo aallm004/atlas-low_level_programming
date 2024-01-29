82fa35532295a26fcff071535f9d6b9536f5dfed
aallm004 <abigailrosea10@gmail.com>
2024-01-29 00:27:20 +0000

    lala

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,11 @@
 #include <string.h>
 
 /**
- * _strlen - function that returns the length of a string
+ * _strlen - returns the length of a string
  *
  * @s : int
+ *
+ * Return: the length of the string
  */
 
 int _strlen(char *s)

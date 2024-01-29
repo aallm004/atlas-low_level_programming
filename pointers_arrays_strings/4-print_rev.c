d82c82f0538ce59ffe53145f9afe8e8ac34b30cf
aallm004 <abigailrosea10@gmail.com>
2024-01-29 02:16:26 +0000

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, w new line.
+ */
+
+void print_rev(char *s)
+{
+	int len;
+	int pos;
+
+	len = _strlen(s);
+
+
+	for ( pos = -1; pos >= 0; pos -- )
+	{
+		_putchar (s[pos]);
+		_putchar ('\n')
+}
+	
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s : int
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	return (x);
+}

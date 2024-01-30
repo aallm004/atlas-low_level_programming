51d639c8268663cba4ba5057bc5016c779033664
aallm004 <abigailrosea10@gmail.com>
2024-01-30 01:20:14 +0000

    duh

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - f that copies the s pointed by src
+ */
+
+char *_strcpr(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+		returnd dest;
+}

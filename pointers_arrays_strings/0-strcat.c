dfb1134955ededc9eb152120fbf856a57969af6f
aallm004 <abigailrosea10@gmail.com>
2024-01-31 20:37:17 +0000

    ala

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,13 @@ char *_strcat(char *dest, char *src)
 	int a, b;
 
 	a = 0;
-	while( dest[a] != '\0')
+	while(dest[a] != '\0')
 	{
 		a++;
 	}
 
 	b = 0;
-	while( src[b] != '\0')
+	while(src[b] != '\0')
 	{
 		dest[a] = src[b];
 		a++;

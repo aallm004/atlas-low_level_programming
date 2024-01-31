7a8e4a6e5d123e8cd8c68dde1548d351d5961c87
aallm004 <abigailrosea10@gmail.com>
2024-01-31 20:11:57 +0000

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - write a function that concatenates two stings
+ * dest: destination
+ * src: string
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int a, b;
+
+	for (a = 0; *dest[a] != '\0'; a++)
+	
+	for (b = 0; *src[b] != '\0'; b++)
+
+	{
+		dest[a]=src[b];
+	}
+	dest[a]='\0';
+
+	return 0;
+}

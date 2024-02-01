5a6f73eb274b38816e2f8a54ea9f52c0889dff9c
aallm004 <abigailrosea10@gmail.com>
2024-02-01 22:24:21 +0000

    4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * reverse_array - function that rev content of an array
+ *
+ * @n: # of elements of array
+ */
+
+void reverse_array(int *a, int n)
+
+{
+	int i, tmp;
+		
+		for (i = 0; i < n/2; i++)
+		{
+			tmp = a[i];
+			a[i] = a[ n - 1 - i];
+			a [n - 1 - i ] = tmp;
+		}
+}

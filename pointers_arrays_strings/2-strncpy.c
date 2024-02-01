cb778efaed854f8f819626748ca34d6438b61510
aallm004 <abigailrosea10@gmail.com>
2024-02-01 19:40:32 +0000

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *
+ * _strncpy - write a function that copies a string
+ *
+ * @dest : destination
+ * @src : string
+ * @n : end
+ *
+ * Return : Always 0.
+ */
+ 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[a] = src[a];
+
+	for (n > a; a++)
+		dest[a] = '\0';
+
+	return (dest);
+}

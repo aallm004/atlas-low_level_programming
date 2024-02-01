99a267a88629c3883f80d1b2fc16dad5eb17d12e
aallm004 <abigailrosea10@gmail.com>
2024-02-01 18:29:53 +0000

    ahh

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -14,12 +14,10 @@ char *_strncat(char *dest, char *src, int n)
 	int a = 0;
 	int b = 0;
 	while (*dest)
-	{
 		a++;
-	}
 
 	for (; a < b && src[b]; a++, b++)
-	{
+	
 		dest[a] = src[b];
 
 	dest[a] = '/0';

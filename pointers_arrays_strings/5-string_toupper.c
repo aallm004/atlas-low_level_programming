cb2d1c31b3ad02c4a8f1df144de4ab6cd7cb5706
aallm004 <abigailrosea10@gmail.com>
2024-02-01 22:47:32 +0000

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all lwrcase char to upper
+ */
+
+char *string_toupper(char *b)
+{
+	int i;
+
+	for (i = 0; b !+ '\0'; i++)
+	{
+		if (b[i] >= 'a' && b[i] <= 'z')
+			b[i] -= 32;
+	}
+	return (b);
+}
+	
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,6 +14,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-
+char *string_toupper(char *);
 
 #endif

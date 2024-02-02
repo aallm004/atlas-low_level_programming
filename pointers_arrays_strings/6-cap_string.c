a5185207d1bc58a8245062ec4724fdbedb2c0552
aallm004 <abigailrosea10@gmail.com>
2024-02-02 21:30:34 +0000

    last

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -4,39 +4,12 @@
  * cap_string- funct that caps all words of string
  */
 
-char *cap_string(char *danny)
+char *cap_string(char *i)
 {
-	int danny;
+	int i = 0;
+	char find[13] ',',';','.','!','?','\"','(',')','{','}','\t','\n',' ';
 
-	for (danny = 0; danny[i] != '\0'; i++)
-	{
-		if(i == 0 && (danny[i] >= 'a' && danny[i] <= 'z'))
-		{	
-			if(i == 0)
-			{
-				danny[i] -= 32;	
-			}
-		
-			else if
-			(
-			danny[i - 1] == ' ' ||
-			danny[i - 1] == ',' ||
-			danny[i - 1] == ';' ||
-			danny[i - 1] == '.' ||
-			danny[i - 1] == '!' ||
-			danny[i - 1] == '?' ||
-			danny[i - 1] == '"' ||
-			danny[i - 1] == '(' ||
-			danny[i - 1] == ')' ||
-			danny[i - 1] == '{' ||
-			danny[i - 1] == '}' ||
-			danny[i - 1] == '\t' ||
-			danny[i - 1] == '\n'
-			)
-			{
-			danny[i] -= 32;
-			}
-		}
-	}
-	return (danny);
+	while (*i != 0; i++)
+
+	return (i);
 }

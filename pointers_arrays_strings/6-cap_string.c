6d527650787a69bb4e2283b846ed2f0bb1678b49
aallm004 <abigailrosea10@gmail.com>
2024-02-02 20:20:50 +0000

    look

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,25 +11,32 @@ char *cap_string(char *danny)
 	for (i = 0; danny[i] != '\0'; i++)
 	{
 		if(i == 0 && (danny[i] >= 'a' && danny[i] <= 'z'))
-		if(i == 0)
-		{
-			danny[i] -= 32;	
+		{	
+			if(i == 0)
+			{
+				danny[i] -= 32;	
+			}
+		
+			else if
+			(
+			danny[i - 1] == ' ' ||
+			danny[i - 1] == ',' ||
+			danny[i - 1] == ';' ||
+			danny[i - 1] == '.' ||
+			danny[i - 1] == '!' ||
+			danny[i - 1] == '?' ||
+			danny[i - 1] == '"' ||
+			danny[i - 1] == '(' ||
+			danny[i - 1] == ')' ||
+			danny[i - 1] == '{' ||
+			danny[i - 1] == '}' ||
+			danny[i - 1] == '\t' ||
+			danny[i - 1] == '\n'
+			)
+			{
+			danny[i] -= 32;
+			}
 		}
-		else if(
-
-	danny[i - 1] == ' ' ||
-	danny[i - 1] == ',' ||
-	danny[i - 1] == ';' ||
-	danny[i - 1] == '.' ||
-	danny[i - 1] == '!' ||
-	danny[i - 1] == '?' ||
-	danny[i - 1] == '"' ||
-	danny[i - 1] == '(' ||
-	danny[i - 1] == ')' ||
-	danny[i - 1] == '{' ||
-	danny[i - 1] == '}' ||
-	danny[i - 1] == '\t' ||
-	danny[i - 1] == '\n')
 	}
 	return (danny);
 }

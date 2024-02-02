0a5897532606e547a01395e99941907c4c5f9444
aallm004 <abigailrosea10@gmail.com>
2024-02-02 23:11:07 +0000

    la

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,6 +15,6 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *string_toupper(char *);
-char *cap_string(char *);
+char *cap_string(char *i);
 char *leet(char *);
 #endif

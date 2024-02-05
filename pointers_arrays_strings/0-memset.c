1dec7cb812696b511820593eede3887727970112
aallm004 <abigailrosea10@gmail.com>
2024-02-05 15:44:18 +0000

    first push

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * memset- function that fills memory with a constant byte
+ *
+ *@n : int
+ *@b : byte
+ *@s : pointer
+ *
+ * Return: pointer to @s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+	
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+
+	return (s);
+}

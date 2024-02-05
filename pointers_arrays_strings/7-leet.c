86b15b68116db7c71cf26b165e5187f8ec025cd7
aallm004 <abigailrosea10@gmail.com>
2024-02-05 02:58:17 +0000

    finished?

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,3 +6,22 @@
 
 char *leet(char *s)
 {
+	int howmanytimesdidwemoveit;
+	char MDA[5][3] = {{'a','A','4'},{'e','E','3'},{'o','O','0'},{'t','T','7'},{'l','L','1'}};
+	int r;
+
+	while(*s)
+	{
+		for (r=0; r<=4; r++)
+		{
+			if((MDA[r][0] == *s) || (MDA[r][1] == *s))
+				*s = MDA[r][2];
+		}
+		
+		s++;
+		howmanytimesdidwemoveit--;
+
+	}
+	s += howmanytimesdidwemoveit;
+	return (s);
+}

9358416479135bb262f2645d4a4bb0b12619c395
aallm004 <abigailrosea10@gmail.com>
2024-02-05 22:04:16 +0000

    first 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ *
+ *@c: character
+ *@s: string
+ *@NULL: end of string
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+		return s;
+		}
+		s++;
+	}
+	return NULL;
+}
+

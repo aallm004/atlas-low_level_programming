8db49c662b2e7f5c8f30e7e1c2a3ddb3074a8fd1
aallm004 <abigailrosea10@gmail.com>
2024-02-05 03:23:07 +0000

    betty

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,18 +9,18 @@ char *leet(char *s)
 {
 	int howmanytimesdidwemoveit = 0;
 	char MDA[5][3] = {
-		{'a', 'A', '4'}, 
-		{'e', 'E', '3'}, 
-		{'o', 'O', '0'}, 
-		{'t', 'T', '7'}, 
+		{'a', 'A', '4'},
+		{'e', 'E', '3'},
+		{'o', 'O', '0'},
+		{'t', 'T', '7'},
 		{'l', 'L', '1'}};
 	int r;
 
-	while(*s)
+	while (*s)
 	{
-		for (r=0; r<=4; r++)
+		for (r=0; r <= 4; r++)
 		{
-			if((MDA[r][0] == *s) || (MDA[r][1] == *s))
+			if ((MDA[r][0] == *s) || (MDA[r][1] == *s))
 				*s = MDA[r][2];
 		}
 

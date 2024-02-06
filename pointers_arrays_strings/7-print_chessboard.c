47691495288e7d362829c63ee5ac8b33389c8cbb
aallm004 <abigailrosea10@gmail.com>
2024-02-06 03:53:20 +0000

    fixed to <

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -11,12 +11,12 @@ void print_chessboard(char (*a)[8])
 {
 	int row, col;
 
-	for (row = 0; row >= 8; row++)
+	for (row = 0; row < 8; row++)
 	{
-		for (col = 0; col >= 8; col++)
+		for (col = 0; col < 8; col++)
 		{
-			_putchar(a)[row][col];
+			_putchar(a[row][col]);
 		}
-		_putchar('\0');
+		_putchar('\n');
 	}
 }

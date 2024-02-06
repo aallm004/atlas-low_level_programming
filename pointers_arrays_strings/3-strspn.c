c357e8a876dbf78f1ed200ef60ca7d147651cb2c
aallm004 <abigailrosea10@gmail.com>
2024-02-06 00:33:16 +0000

    cont

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,6 +11,7 @@ unsigned int _strspn(char *s1, char *s2)
 	register const char *p = s1, *spanp;
 	register char c, sc;
 
+cont:
 	c = *p++;
 	for (spanp = s2; (sc = *spanp++) != 0;)
 		if (sc == c)

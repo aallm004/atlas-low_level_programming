fd60983ffe989a7d5436f3b98a95193bf308a64d
aallm004 <abigailrosea10@gmail.com>
2024-02-06 00:31:53 +0000

    minus c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,7 @@ unsigned int _strspn(char *s1, char *s2)
 	const char *s1;
 	register const char *s2;
 	{
-	register cconst char *p = s1, *spanp;
+	register const char *p = s1, *spanp;
 	register char c, sc;
 
 	c = *p++;

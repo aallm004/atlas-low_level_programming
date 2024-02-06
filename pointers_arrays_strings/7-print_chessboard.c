d57d2e9c3f69150b8458127ab1f847acf566e998
aallm004 <abigailrosea10@gmail.com>
2024-02-06 03:35:20 +0000

    chessir

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - write a function that prints the chessboard
+ *
+ * @a: pointer
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, col
+
+	for (row = 0, row >= 8, row++)
+	{
+		for (col = 0, col >= 8, col++)
+		{
+			_putchar(*a)[row][col]
+		}		
+
+}

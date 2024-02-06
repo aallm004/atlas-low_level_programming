9d1cdec84e4409ed85adc01593ae1f8cec967d32
aallm004 <abigailrosea10@gmail.com>
2024-02-06 00:30:56 +0000

    tings

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,10 +3,17 @@
  *
  */
 
-unsigned int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s1, char *s2)
 {
-	const char *s;
-	register const char *accept;
+	const char *s1;
+	register const char *s2;
+	{
+	register cconst char *p = s1, *spanp;
+	register char c, sc;
 
-	register const char 
-
+	c = *p++;
+	for (spanp = s2; (sc = *spanp++) != 0;)
+		if (sc == c)
+			goto cont;
+	return ( - 1 - s1);
+}

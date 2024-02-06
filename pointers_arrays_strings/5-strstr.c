e7a796e7df115ae1c975628cc1b10d92e2a6ed4c
aallm004 <abigailrosea10@gmail.com>
2024-02-06 00:58:34 +0000

    first

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+/**
+ * _strstr - function that locates a substring
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, l1, l2;
+	int count = 0;
+	l1 = _strlen ( string );
+	l2 = _strlen ( substring );
+
+	for (i = 0; i < l1 - l2 + 1; i++)
+	{
+		count++;
+		i = i + l2 - 1;
+	}
+}
+return (count);
+
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s : int
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	return (x);
+}

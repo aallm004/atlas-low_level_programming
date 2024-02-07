3e27c458bc99d9f782fb1d431b51f665f708e18c
aallm004 <abigailrosea10@gmail.com>
2024-02-07 21:37:19 +0000

    strlenagain

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * strlen_recursion - function that returns the length of a string
+ *
+ * @s: string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int temp;
+	int len;
+
+	len = strlen(s);
+	if ((len + 1) / 2 > n)
+		reverse(s, n + 1_;
+
+	temp = s[n - 1]
+	s[n - 1] = s[(len - 1) - (n - 1)];
+	s[(len - 1) - (n - 1)] = temp;
+}

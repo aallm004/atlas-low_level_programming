22e4b076510afec752a8770622a946ce96e3f7bf
aallm004 <abigailrosea10@gmail.com>
2024-02-07 20:20:19 +0000

    trying again

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,10 +7,12 @@ void _puts_recursion(char *s)
 {
 	if (*s == 0)
 	{
-		_putchar('\n');
-		return;
+		_putchar ('\n');
 	}
-	_putchar(s);
-	s++;
-	_puts_recursion(s);
+	else
+	{
+		_putchar (s);
+		s++;
+		(_puts_recursion(s));
+	}
 }

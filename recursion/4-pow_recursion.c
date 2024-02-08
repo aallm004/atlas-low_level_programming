4387e08cc196c0c2270466da87fd944ac4bc9675
aallm004 <abigailrosea10@gmail.com>
2024-02-08 02:21:06 +0000

    power

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - funct returns v of x raised to pwr y
+ *
+ * @x: int
+ * @y: power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (x == 0)
+	{
+		return (0);
+	}
+	return x * _pow_recursion(y, x - 1);
+}

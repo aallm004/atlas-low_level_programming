6a3dffd1651987d5b4636c14f6050bf4b679bd3f
aallm004 <abigailrosea10@gmail.com>
2024-02-08 00:55:43 +0000

    betty

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,16 +3,17 @@
  * strlen_recursion - function that returns the length of a string
  *
  * @s: string
+ * @x: int
+ * Return - int
  */
-
 int _strlen_recursion(char *s)
 {
 	int x = 0;
-	
+
 	if (*s)
 	{
-                x++;
-                x += _strlen_recursion(s + 1);
-        }
+		x++;
+		x += _strlen_recursion(s + 1);
+	}
 	return (x);
 }

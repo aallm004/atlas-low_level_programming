ebc92f11f7b0ddde06435c806e0f4153269c0db5
aallm004 <abigailrosea10@gmail.com>
2024-02-08 02:36:38 +0000

    back to last

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,10 +8,12 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
+	if (y != 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
 	{
 		return (1);
 	}
-
-	return x * _pow_recursion(x, (y - 1));
 }

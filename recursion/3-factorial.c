4f0b9564f30df7ba71b6abe279a264fcc0694524
aallm004 <abigailrosea10@gmail.com>
2024-02-08 01:32:40 +0000

    factorialio

---
+++ recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - returns factorial of given number
+ *
+ * @n : int
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n* factorial(n-1));
+ 	}
+}	

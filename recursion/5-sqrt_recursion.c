05fd33b2c1bef2dd8b1105ab53c3324cbbf00eb1
aallm004 <abigailrosea10@gmail.com>
2024-02-09 17:29:23 +0000

    betty

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,8 +5,10 @@ int _sqrt_recursion_worker(int a, int b);
 /**
  * _sqrt_recursion - function that returns natural square root of a number
  *
+ * _sqrt_recursion_worker - extra funtion
  * @n: root
- * @i: int
+ * @a: bla
+ * @b: bla
  */
 
 int _sqrt_recursion(int n)

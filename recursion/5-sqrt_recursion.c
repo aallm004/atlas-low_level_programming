4939486a1757737a0cef6c7d01fe2cd7a5ca7f13
aallm004 <abigailrosea10@gmail.com>
2024-02-09 17:24:10 +0000

    added funciton

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int _sqrt_recursion_worker(int a, int b);
+
 /**
  * _sqrt_recursion - function that returns natural square root of a number
  *
@@ -15,14 +17,14 @@ int _sqrt_recursion(int n)
 int _sqrt_recursion_worker(int a, int b)
 {
 
-        if (b * b > i)
-        {
-                return (-1);
-        }
-        else if (b * b == a)
-        {
-                return (b);
-        }
-        else
-                return (_sqrt_recursion(a, b + 1));
+	if (b * b > a)
+	{
+		return (-1);
+	}
+	else if (b * b == a)
+	{
+		return (b);
+	}
+	else
+		return (_sqrt_recursion(a, b + 1));
 }

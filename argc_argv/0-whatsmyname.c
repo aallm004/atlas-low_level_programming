af477e8a68c3059489f875a31e323a861f809d9e
aallm004 <abigailrosea10@gmail.com>
2024-02-10 20:13:41 +0000

    *

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,12 +6,11 @@
  * @argc: count
  * @argv: value
  *
- * Return: Always 0
+ * Return: Always 0 (always success)
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n" argv[0]);
-	
+	printf("%s\n" *argv[0]);
 	return (0);
 }

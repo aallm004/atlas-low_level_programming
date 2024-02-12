1796b4b0fccc6814f6ed1c4fe5e756f37c6ca951
aallm004~ <abigailrosea10@gmail.com>
2024-02-12 21:39:33 +0000

    litcherally everything

---
+++ argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+int isnumber(char *ptr);
+/**
+ * main - program that adds positive numbers
+ * @argv: count
+ * @argv: value
+ *
+ * Return: Always 0 (always success)
+ */
+
+int main(int argc, char *argv[])
+{
+        int i = 1;
+	int res = 0;
+
+	for (; (i < argc); i++)
+	{
+		if (isnumber(argv[i]))
+			res += (atoi(argv[i]));
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%i\n", res);
+}
+
+/**
+ * isnumber - is it a number
+ * @ptr: point
+ *
+ * Return: 0 if not num 1 if num
+ */
+
+int isnumber(char *ptr)
+{
+        while (*ptr)
+        {
+		if (!isdigit(*ptr))
+			return (0);
+		ptr++;
+	}
+	return (1);
+}

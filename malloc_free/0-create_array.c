aea0317f4f5a5f502fbfdae6d503ed1a6194af53
aallm004~ <abigailrosea10@gmail.com>
2024-02-14 17:57:22 +0000

    stupid main

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
  * create_array - function that creates an array of chars and init w specific char
@@ -19,5 +20,5 @@ char *create_array(unsigned int size, char c)
 	for (i=0; i < size; i++)
 		array[i] = c;
 
-	return (i)
+	return (array);
 }

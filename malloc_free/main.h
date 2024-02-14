6877050e7fe0acd2e67d18c0833d3d0ea52b718a
aallm004 <abigailrosea10@gmail.com>
2024-02-14 21:47:49 +0000

    copy

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -4,6 +4,6 @@
 char *create_array(unsigned int size, char c);
 int _strlen(char *s);
 char *_strdup(char *str);
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n);
 
 #endif

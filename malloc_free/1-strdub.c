a256c5048ea3ae9d1727587e91a86b794e410bd1
aallm004~ <abigailrosea10@gmail.com>
2024-02-14 21:02:51 +0000

    push

--- malloc_free/1-strdub.c
+++ malloc_free/1-strdub.c
@@ -11,9 +11,18 @@
 
 char *_strdub(char *str)
 {
-	int len = _strlen(str)
+	int len = _strlen(str);
+	int i;
 
+	if (strlen == 0)
+		return (NULL);
 
+	str = malloc(_strlen * str);
+		if (str == 0)
+			return (NULL);
+
+	for (i = 0; i < _strlen; i++)
+		str[i]
 
 }
 
@@ -40,4 +49,29 @@ int _strlen(char *s)
 	}
 
 	return (x);
+}
+
+/**
+ * _strncpy - write a function that copies a string
+ *
+ * @dest : destination
+ * @src : string
+ * @n : end
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0' && a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+	for (; a < n; a++)
+
+	dest[a] = '\0';
+
+	return (dest);
 }

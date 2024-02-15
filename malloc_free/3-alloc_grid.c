c521afbe6ca643e0164ebf7e028fe5bf2372701f
aallm004 <abigailrosea10@gmail.com>
2024-02-15 19:20:03 +0000

    have to go

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - return pointer to a 2 dimensional array of int
+ *
+ * @width: width
+ * @height: height
+ */
+
+int **alloc_grid(int width, int height)
+
+

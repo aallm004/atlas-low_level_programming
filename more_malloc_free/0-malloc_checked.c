56e90750a5d384a2116179fa7f4332b8a13aac0b
aallm004 <abigailrosea10@gmail.com>
2024-02-16 19:19:45 +0000

    malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - function that locates memory using malloc
+ *
+ * @b: unsigned int
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *x;
+
+	x = malloc(b * sizeof(int))
+	if (x == 0)
+		exit(98);
+	return (b);
+}

fb64f7f71ff7c21ab1ee28499efd32ef2147cd34
aallm004 <abigailrosea10@gmail.com>
2024-02-22 17:52:59 +0000

    switched name and age

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,15 +13,14 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 		{
-			if (d->age)
-				printf("Age: %.6f\n", d->age);
-			else
-				printf("Age: (nil)\n");
-
 			if (d->name != NULL)
 				printf("Name: %s\n", d->name);
 			else
-				printf("Name: (nil)\n");
+
+			if (d->age)
+                                printf("Age: %.6f\n", d->age);
+                        else
+                                printf("Age: (nil)\n");
 
 			if (d->owner != NULL)
 				printf("Owner: %s\n", d->owner);

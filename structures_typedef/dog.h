30d65ef5ac4ea0a7901b1c2126233089c718cfdc
aallm004 <abigailrosea10@gmail.com>
2024-02-22 14:07:44 +0000

    first struct with.. header?

---
+++ structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - information about dog
+ *
+ * @name: char name of dog
+ * @age: float age of dog
+ * @owner: char name of owners
+ *
+ * Description: Defining a new type
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif

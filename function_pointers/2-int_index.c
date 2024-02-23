8fb4d0f43c7b2100783389b63f216f13c03796e3
aallm004 <abigailrosea10@gmail.com>
2024-02-23 17:13:41 +0000

    eh?

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,10 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if(size <= 0 || array == 0 || size == 0)
+	if (size <= 0 || array == 0 || size == 0)
 		return (-1);
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		(cmp(array[i]));
 		 return (i);

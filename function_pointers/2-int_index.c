5be16f85378f4677de9502e670f3a290addf5ab7
aallm004 <abigailrosea10@gmail.com>
2024-02-23 17:08:19 +0000

    integer search

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ * @array: array to be searched
+ * @size: number of elements in array
+ * @cmp: function ptr
+ * 
+ * Return: index of the first element, -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if(size <= 0 || array == 0 || size == 0)
+		return (-1);
+
+	for(i = 0; i < size; i++)
+	{
+		(cmp(array[i]));
+		 return (i);
+	}
+
+	return (-1);
+}

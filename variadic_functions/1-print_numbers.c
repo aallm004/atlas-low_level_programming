653ec30366d59cc040dd05610f22fbf8fc66ae0e
aallm004 <abigailrosea10@gmail.com>
2024-02-23 20:51:12 +0000

    fin for now

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - function that prints numbers, followed by new line
+ * @separator: string
+ * @n: int
+ */
+
+void print_numbers(const char *separator, const unsigned int n,...)
+
+

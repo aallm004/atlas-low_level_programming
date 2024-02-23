ec5ce67d70eed6bb2eb9a6d9c7724b8c555f164e
aallm004 <abigailrosea10@gmail.com>
2024-02-23 16:36:12 +0000

    print

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ *
+ * @name: name to be printeid
+ * @f: pointer
+ *
+ * Return: nothing
+ */
+
+void print_nme(char *name, void (*f)(char *))
+{
+	f(name);
+}
+

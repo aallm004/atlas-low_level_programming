f9b7b8244baff51d357a9229fdb07176371cc363
aallm004 <abigailrosea10@gmail.com>
2024-02-23 20:33:34 +0000

    betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,9 +12,9 @@ int sum_them_all(const unsigned int n, ...)
 	va_list nums;
 
 	if (n == 0)
-	return 0;
+	return (0);
 
-	va_start (nums, n);
+	va_start(nums, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(nums, int);
 

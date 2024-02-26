6e82b17a86382c78e9dde167e13f1beae750f60b
aallm004 <abigailrosea10@gmail.com>
2024-02-26 18:24:43 +0000

    ruiiiined

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -22,10 +22,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		s = va_arg(ap, char *);
 		if (s == NULL)
 			printf("(nil)");
-			printf("%s", s);
-
+		printf("%s", s);
 		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
+	va_end(ap);
 }

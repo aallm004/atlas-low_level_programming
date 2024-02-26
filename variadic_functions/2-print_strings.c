c5bcfaa9fd7ca4e0a2bc17d7bfae54565ef16baa
aallm004 <abigailrosea10@gmail.com>
2024-02-26 18:26:49 +0000

    NOT equal

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(ap, char *);
-		if (s == NULL)
+		if (s != NULL)
 			printf("nil");
 		printf("%s", s);
 		if (separator != NULL && i != (n - 1))

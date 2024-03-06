88f4bbc50842e7252b96408707018b7d93223a80
aallm004 <abigailrosea10@gmail.com>
2024-03-06 22:53:49 +0000

    leaving skool

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * add_node - add new node at beginning of a list_t list
+ *
+ *@head: top of list
+ *@str: string to be added
+ *
+ * Return: address of new element, NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+
+
+
+	if (head == NULL)
+		return (
+
+

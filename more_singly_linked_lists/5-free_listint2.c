7634c75f86edab694dde95ef961c713e51c3406a
aallm004 <abigailrosea10@gmail.com>
2024-03-07 23:45:55 +0000

    first

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a list
+ *@head: head of list
+ *
+ * Return: nothing
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_node = NULL;
+
+	while (*head != NULL)
+	{
+	temp_node = *head;
+	*head = (*head)->next;
+	free(temp_node);
+	}
+}

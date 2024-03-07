ae9d8710e4f569ed9bcb3e416bb1457a3c4b7f9a
aallm004 <abigailrosea10@gmail.com>
2024-03-07 18:41:20 +0000

    first push

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ *
+ * @head: top of list
+ *
+ *Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp_node;
+
+	while(head)
+	{
+	temp_node = head;
+	head = head->next;
+	free(temp_node);
+	}
+}

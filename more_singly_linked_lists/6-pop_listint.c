8e2d637bf10eab11fee318d15af4e33841aedcff
aallm004 <abigailrosea10@gmail.com>
2024-03-08 00:14:02 +0000

    finished for tonight

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * pop_listint - function deletes head node of list
+ *
+ * @head: first element
+ *
+ * Return: head of node's data, 0
+ */
+
+int pop_listint(listint_t **head)
+

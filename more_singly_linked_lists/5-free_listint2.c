e9ad420e71c86ee238607cca05d199a5f5315070
aallm004 <abigailrosea10@gmail.com>
2024-03-08 22:03:27 +0000

    added if

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -11,10 +11,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp_node = NULL;
 
+	if (head != NULL)
+	{
 	while (*head != NULL && head != NULL)
 	{
 	temp_node = *head;
 	*head = (*head)->next;
 	free(temp_node);
+	}
 	}
 }

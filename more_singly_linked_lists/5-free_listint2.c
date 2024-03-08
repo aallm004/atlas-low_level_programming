de9ffae8cd2838b23e2e5dde5f8cbf707615e2e8
aallm004 <abigailrosea10@gmail.com>
2024-03-08 21:59:16 +0000

    null

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp_node = NULL;
 
-	while (*head != NULL)
+	while (*head != NULL && head != NULL)
 	{
 	temp_node = *head;
 	*head = (*head)->next;
---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - function that inserts a new node at a given position
+ * @head: first element
+ * @idx: unsigned int
+ * @n: int
+ *
+ * Return: NULL
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int idx = 0
+
+}

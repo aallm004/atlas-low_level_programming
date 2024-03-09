dc32c9cd0dd0f2c1567e781b51332fb1c073c784
aallm004 <abigailrosea10@gmail.com>
2024-03-09 22:01:51 +0000

    added ()

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 
-	while (pos < idx - 1 && temp_node != NULL)
+	while (pos < (idx - 1) && temp_node != NULL)
 	{
 		temp_node = temp_node->next;
 		pos++;

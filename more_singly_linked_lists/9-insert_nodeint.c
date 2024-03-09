915c7271f1293691e9ad8ed65051ee7cdb77a81a
aallm004 <abigailrosea10@gmail.com>
2024-03-09 21:55:02 +0000

    push

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -10,6 +10,28 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int idx = 0
+	unsigned int pos = 0;
+	listint_t *temp_node = *head;
+	listint_t *new_node;
+	
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
+	new_node->n = n;
+
+	while (temp_node != NULL && pos < (idx - 1))
+	{
+		temp_node = temp_node->next;
+		pos++;
+	}
+
+	if (pos == (idx - 1))
+	{
+		new_node->next = temp_node->next;
+		temp_node->next = new_node;
+		return (new_node);
+	}
+
+	return(NULL);
 }

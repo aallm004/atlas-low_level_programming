7236d33683dfc31fa6ce8a14aa0f0813f6b460b8
aallm004 <abigailrosea10@gmail.com>
2024-03-09 22:38:59 +0000

    addnodeint

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -14,19 +14,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp_node = *head;
 	listint_t *new_node;
 
+	if (idx == 0)
+		return (add_nodeint(head, n));
+		
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
 
-	while (pos < (idx - 1) && (temp_node != NULL) && (idx != 0))
+	while (pos < (idx - 1) && (temp_node != NULL))
 	{
 		temp_node = temp_node->next;
 		pos++;
 	}
 
-	if (pos == (idx - 1) || (idx == 0))
+	if (pos == (idx - 1))
 	{
 		new_node->next = temp_node->next;
 		temp_node->next = new_node;
@@ -34,4 +37,31 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	return (NULL);
+}
+
+/**
+ * add_nodeint - function adds new node at beginning of list
+ * @head: top
+ * @n: int
+ *
+ * Return: addrdss of new element, NULL
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = NULL;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (*head);
 }

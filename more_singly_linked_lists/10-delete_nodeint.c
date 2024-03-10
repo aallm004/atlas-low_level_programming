5274adcc5f6c1761aa3f580e1502e6f6f5fcb523
aallm004 <abigailrosea10@gmail.com>
2024-03-10 01:16:01 +0000

    trying

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index
+ *@head: first element
+ *@index: space
+ *
+ * Return: 1, -1
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int pos = 0;
+	listint_t *temp_node = *head;
+	listint_t *del_node = NULL;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp_node);
+		return (1);
+	}
+
+	while (pos < (index - 1) && (temp_node != NULL))
+	{
+		temp_node = temp_node->next;
+		pos++;
+	}
+
+	del_node = temp_node->next;
+	free(del_node);
+
+	return (1);
+}

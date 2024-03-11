c377a8bb4fcddfbf7da1f400e2b7f06c5e93d8c7
aallm004 <abigailrosea10@gmail.com>
2024-03-11 17:29:47 +0000

    movin

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * set_bit - function sets value of bit to 1 at given index
+ *
+ * @index: index
+ * @n: pointer
+ *
+ * Return: 1, -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int hole = 1;
+
+	if (n == 0)
+	{
+		return (-1);
+	}
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+	
+	hole <<= index;
+
+	*n = hole;
+
+		return (1);
+}

b1867ce46a17e19094ee4669541ba330a3ab5b94
aallm004 <abigailrosea10@gmail.com>
2024-03-11 02:08:36 +0000

    int numbbbbs

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -2,8 +2,8 @@
 /**
  * get_bit - function that returns the value of a bit at given index
  *
- *@:n - int
- *@:index: index
+ *@n: int
+ *@index index
  *Return: value of bit, null
  */
 
@@ -14,6 +14,11 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n == 0)
 	{
 		return (0);
+	}
+
+	if (index > 31)
+	{
+		return (-1);
 	}
 
 	hole = (hole << index);

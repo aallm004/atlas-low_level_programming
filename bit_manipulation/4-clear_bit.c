b8a6fce2a7fa536d3a0aa4879adc315a83a74844
aallm004 <abigailrosea10@gmail.com>
2024-03-11 17:46:58 +0000

    moving spaces

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * clear_bit - function that sets value of bit to 0 at given index
+ *
+ * @n: pointer
+ * @index: index
+ *
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+}
---
+++ bit_manipulation/5-flib_bits.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * flip_bits - function that returns num of bits needed to flip to go
+ *
+ * @n: int
+ * @m: long int
+ *
+ * Return: number of bits you would need
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+}

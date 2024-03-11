3f6eec0172242d71437dc10e4f51ebf855f0b1c4
aallm004 <abigailrosea10@gmail.com>
2024-03-11 01:58:40 +0000

    value of bit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * get_bit - function that returns the value of a bit at given index
+ *
+ *@:n - int
+ *@:index: index
+ *Return: value of bit, null
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int hole = 1;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	hole = (hole << index);
+
+	if (hole & n)
+	{
+		return (1);
+	}
+
+	return (0);
+
+}

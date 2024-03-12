ad85154a0e8dc209fa3e4de38dfff58ab5e73100
aallm004 <abigailrosea10@gmail.com>
2024-03-12 01:27:28 +0000

    misplaced code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,16 +16,14 @@ int create_file(const char *filename, char *text_content)
 		return (0);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        if (fd == -1)
+                return (-1);
 
 	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
-
-	if (fd == -1)
-		return (-1);
-
 	for (i = 0; text_content[i]; i++)
 	;
 

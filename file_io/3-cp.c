275fd71b3eb86e56500154719c9f02564dd1c4f9
aallm004 <abigailrosea10@gmail.com>
2024-03-12 02:32:30 +0000

    written

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -60,14 +60,14 @@ int main(int argc, char *argv[])
 
 	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Unable to close fd %d\n", fd_from);
 		close(fd_to);
 		return (100);
 	}
 
 	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Unable to close fd %d\n", fd_to);
 		return (100);
 	}
 

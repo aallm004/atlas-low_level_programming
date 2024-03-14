6bc2800f6a87da3fdaba57166eae8d0eeea16179
aallm004 <abigailrosea10@gmail.com>
2024-03-14 20:21:42 +0000

    indentwho

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,11 +25,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		temp_node = *head;
-	while (temp_node->next)
+		while (temp_node->next)
 				temp_node = temp_node->next;
 
-			temp_node->next = new_node;
-			new_node->prev = temp_node;
+		temp_node->next = new_node;
+		new_node->prev = temp_node;
 
 	}
 	return (new_node);

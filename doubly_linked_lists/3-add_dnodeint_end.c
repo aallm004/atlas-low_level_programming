dd3be0c5970838c9fa7935ea55c3ff6eaa323f91
aallm004 <abigailrosea10@gmail.com>
2024-03-14 20:17:27 +0000

    while

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		temp_node = *head;
-		while (temp_node->next != NULL)
+		while (temp_node->next)
 				temp_node = temp_node->next;
 
 			temp_node->next = new_node;

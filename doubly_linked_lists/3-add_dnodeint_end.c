e1eaded601b6a895a4cd8dbcab021c578c0524c5
aallm004 <abigailrosea10@gmail.com>
2024-03-14 20:23:18 +0000

    too far in

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -26,7 +26,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		temp_node = *head;
 		while (temp_node->next)
-				temp_node = temp_node->next;
+			temp_node = temp_node->next;
 
 		temp_node->next = new_node;
 		new_node->prev = temp_node;

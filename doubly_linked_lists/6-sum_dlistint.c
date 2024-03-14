b5c192cd4d1a5eefb6f56bf6ec6658761ca04ca9
aallm004 <abigailrosea10@gmail.com>
2024-03-14 23:04:07 +0000

    betty being a jerk

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
  * sum-dlistint - function that returns the sum of all data in list
- * 
+ *
  * @head: start of function
  *
- * Return: 0 or sum of list
+ * Return: 0, sum
 */
 
 int sum_dlistint(dlistint_t *head)

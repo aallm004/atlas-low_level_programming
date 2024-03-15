f12faba196495d887ab9d1cea34e5d06ecd9432c
aallm004 <abigailrosea10@gmail.com>
2024-03-15 14:56:47 +0000

    all sorts of directions

--- doubly_linked_lists/7-insert-dnodeint.c
+++ doubly_linked_lists/7-insert-dnodeint.c
@@ -10,4 +10,35 @@
 */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+    unsinged int pos = 0;
+    linstint_t *temp_node = *head;
+    listint_t *new_node;
 
+    if (idx == 0)
+        return (add_nodeint(head, n));
+
+    new_node = malloc(sizeof(listint_ta));
+        if (new_node == NULL)
+            return (NULL);
+
+    new_node-> = n;
+
+    while (pos < (idx - 1) && (temp_node != NULL))
+    {
+        temp_node = temp_node->next;
+        pos++;
+    }
+
+    if (pos == (idx - 1))
+    {
+        temp_node->prev->next = new_node;
+        new_node->prev = temp_node->prev;
+        new_node->next = temp_node;
+        temp_node->prev = new_node;
+        
+        return (new_node);
+    }
+
+    return (NULL);
+}

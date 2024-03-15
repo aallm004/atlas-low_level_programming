ae3f5629d57410473b2768d9e02d0679419df7c9
aallm004 <abigailrosea10@gmail.com>
2024-03-15 19:27:11 +0000

    Reaching in the dark

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -19,7 +19,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		temp_node = *head;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		free(temp_node);
 		return (1);
 	}

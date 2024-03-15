99e0945f04879eee7a89b9dca777e23487e7655e
aallm004 <abigailrosea10@gmail.com>
2024-03-15 19:10:18 +0000

    dark

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,7 +11,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int pos = 0;
-	dlistint_t *temp_node = *head;
+	dlistint_t *temp_node = *head
 	if (*head == NULL)
 			return (-1);
 
@@ -24,7 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (pos < (index) && (temp_node != NULL))
+	while (pos != (index) && (temp_node != NULL))
 		{
 			temp_node = temp_node->next;
 			pos++;

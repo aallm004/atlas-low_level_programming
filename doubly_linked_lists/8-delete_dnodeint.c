d128b9a1ea56c8196030a72c1bf2e498991af4f2
aallm004 <abigailrosea10@gmail.com>
2024-03-15 19:29:38 +0000

    added two if's

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -33,8 +33,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 		if (pos == index)
 		{
-			temp_node->prev->next = temp_node->next;
-			temp_node->next->prev = temp_node->prev;
+			if (temp_node->prev != NULL)
+				temp_node->prev->next = temp_node->next;
+			if (temp_node->next != NULL)
+				temp_node->next->prev = temp_node->prev;
 			free(temp_node);
 			return (1);
 		}

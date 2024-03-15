b9665c952532c3fedbeef2c5ef61c927fc90fe28
aallm004 <abigailrosea10@gmail.com>
2024-03-15 22:36:21 +0000

    betty b

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
- * hash_djb2 - hash function implementing djb2 algorithm
- *
- *@str: pointer
+* hash_djb2 - hash function implementing djb2 algorithm
+*
+*@str: pointer
 *Return: nothing
 */
 
@@ -11,10 +11,10 @@ unsigned long int hash_djb2(const unsigned char *str)
     unsigned long int hash;
     int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }

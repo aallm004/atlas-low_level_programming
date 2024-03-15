580720b1c5986791ab2eefe3f9ff303387b59878
aallm004 <abigailrosea10@gmail.com>
2024-03-15 19:08:30 +0000

    semi freaking colon

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -18,8 +18,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		(*head)->next->prev = NULL
-		temp_node = *head
+		(*head)->next->prev = NULL;
+		temp_node = *head;
 		free(temp_node);
 		return (1);
 	}

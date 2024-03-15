89a2f2d14258bfd0c82424357b52561f684d5d7b
aallm004 <abigailrosea10@gmail.com>
2024-03-15 21:20:19 +0000

    walk break

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - hash function implementing djb2 algorithm
+ *
+ *@str: pointer
+*Return: nothing
+*/
+
+unsigned long int hash_djb2(const unsigned char *str)

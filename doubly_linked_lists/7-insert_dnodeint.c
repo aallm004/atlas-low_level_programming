96c4033e7fe7edef514658ef6415809fff9203b5
aallm004 <abigailrosea10@gmail.com>
2024-03-15 15:49:31 +0000

    d end thing

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     dlistint_t *new_node;
 
     if (idx == dlistint_len(*h))
-        return (add_dnodeint_end(h,n));
+        return (add_dnodeint_end(h, n));
 
     if (idx == 0)
         return (add_dnodeint(h, n));
@@ -43,4 +43,40 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     }
 
     return (NULL);
+}
+
+/**
+ * add_dnodeint_end - function that adds new node at end of list
+ *
+ * @head: start of list
+ * @n: holds data
+ *
+ * Return: address of new element or NULL
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+	dlistint_t *temp_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = new_node->prev = NULL;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		temp_node = *head;
+		while (temp_node->next)
+			temp_node = temp_node->next;
+
+		temp_node->next = new_node;
+		new_node->prev = temp_node;
+
+	}
+	return (new_node);
 }

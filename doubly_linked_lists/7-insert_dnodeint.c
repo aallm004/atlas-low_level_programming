8c533b552a89cf191462c42c293e9d13181fb0ad
aallm004 <abigailrosea10@gmail.com>
2024-03-15 15:56:54 +0000

    spaces to tabs

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,48 @@
 #include "lists.h"
 /**
  * insert_dnodeint_at_index - function that inserts new node at given position
- * 
+ *
  * @h: pointer
  * @idx: index
  * @n: int
- * 
+ *
  * Return: address of new node, NULL
 */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    unsigned int pos = 0;
-    dlistint_t *temp_node = *h;
-    dlistint_t *new_node;
+	unsigned int pos = 0;
+	dlistint_t *temp_node = *h;
+	dlistint_t *new_node;
 
-    if (idx == dlistint_len(*h))
-        return (add_dnodeint_end(h, n));
+	if (idx == dlistint_len(*h))
+		return (add_dnodeint_end(h, n));
 
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    new_node = malloc(sizeof(dlistint_t));
-        if (new_node == NULL)
-            return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
 
-    new_node->n = n;
+	new_node->n = n;
 
-    while (pos < (idx - 1) && (temp_node != NULL))
-    {
-        temp_node = temp_node->next;
-        pos++;
-    }
+	while (pos < (idx - 1) && (temp_node != NULL))
+	{
+		temp_node = temp_node->next;
+		pos++;
+	}
 
-    if (pos == (idx - 1))
-    {
-        new_node->prev = temp_node;
+	if (pos == (idx - 1))
+	{
+		new_node->prev = temp_node;
 	new_node->next = temp_node->next;
 	temp_node->next->prev = new_node;
 	temp_node->next = new_node;
-        return (new_node);
-    }
+		return (new_node);
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
 /**

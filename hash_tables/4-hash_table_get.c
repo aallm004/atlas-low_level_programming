806c1dbdf98b91b5fc2b18f906e6de7833939837
aallm004 <abigailrosea10@gmail.com>
2024-03-18 20:27:21 +0000

    push it

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -15,7 +15,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL)
 		return (NULL);
 
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	if (ht->array[index] != NULL)
 		element = ht->array[index];

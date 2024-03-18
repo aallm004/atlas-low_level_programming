ec6bbeeffc86661ac3c02cade38241d9366fd17b
aallm004 <abigailrosea10@gmail.com>
2024-03-18 22:07:18 +0000

    meeeemoriiiiieeesss

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - function that deletes a hash table
+ * @ht: hash table
+ *
+ * Return: nothing
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *element;
+	hash_node_t *temp_bot;
+	unsigned long int index;
+
+	if (ht)
+	{
+		for (index = 0; index < ht->size; index++)
+		{
+			if (ht->array[index] != NULL)
+			{
+				element = ht->array[index];
+
+				while (element)
+				{
+					temp_bot = element;
+					element = element->next;
+					free(temp_bot->key);
+					free(temp_bot->value);
+					free(temp_bot);
+				}
+			}
+		}
+		free(ht);
+	}
+}

db40a59c5928932fa3a84f3847952a30cf76b4f1
aallm004 <abigailrosea10@gmail.com>
2024-04-11 01:27:41 +0000

    added extra printf back

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -16,7 +16,8 @@ int linear_search(int *array, size_t size, int value)
 		for (i = 0; i < size; i++)
 		{
 			printf("Value checked array[%ld] = [%d]", i, array[i]);
-			if (array[i] == value);
+			if (array[i] == value)
+			printf("Found %d at index: %ld\n", value, i);
 		}
 	return (-1);
 

9edb5265e6b812c3ba00bf573c0a35d9d37f332a
aallm004 <abigailrosea10@gmail.com>
2024-04-11 23:29:27 +0000

    if start == end

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -39,7 +39,7 @@ int search(int *array, size_t size, size_t start, size_t end, int value)
 	if (array[half] == value)
 		return (half);
 
-	if (half == 0)
+	if (half == 0 || start == end)
 		return (-1);
 
 	if (array[half] < value)
@@ -47,6 +47,8 @@ int search(int *array, size_t size, size_t start, size_t end, int value)
 
 	else
 		return (search(array, size, start, (half - 1), value));
+
+	return(-1);
 }
 
 /**
@@ -73,4 +75,26 @@ void print_array(int *array, size_t start, size_t end)
 	else
 		printf("\n");
 	}
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * _main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int _main(void)
+{
+    int array[] = {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
+    printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
+    printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
+    return (EXIT_SUCCESS);
 }

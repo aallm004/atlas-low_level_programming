bca57962d7e2d320548f4c2f8c82bb0f9bd8bf2b
aallm004 <abigailrosea10@gmail.com>
2024-04-11 19:19:48 +0000

    0

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+/**
+ *binary_search - function that searches for value in sorted array of ints
+ *
+ *@array: pointer to first element of the array
+ *@size: number of elements in array
+ *@value: value to search for
+ *
+ *Returns: -1, index of value
+ *
+ )*/
+ int binary_search(int *array, size_t size, int value)
+{
+search(binary_search)
+}
+
+/**
+ *search - searching array for value
+ *
+ *@array: array to be searched
+ *@size: size of array
+ *@start: beginning of array
+ *@end: end of array
+ *@value: number to be looked for
+ *
+ *Return: index of value or -1
+ */
+int search(int *array, size_t size, size_t start, size_t end, int value)
+
+
